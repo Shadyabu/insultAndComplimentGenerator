af6e6864a1c59a5c017886a3a73c60953d9c7a69
ec21366 <s.abushady@se21.qmul.ac.uk>
2023-05-06 23:19:08 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [insult, setInsult] = useState('');
+  const [compliment, setCompliment] = useState('');
+
+  const generateInsult = async () => {
+    try {
+      const response = await fetch('https://insult.mattbas.org/api/insult.json');
+      const data = await response.json();
+      setInsult(data.insult);
+      setCompliment('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const generateCompliment = async () => {
+    try {
+      const response = await fetch('https://complimentr.com/api');
+      const data = await response.json();
+      setCompliment(data.compliment);
+      setInsult('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Insult or Compliment</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={generateInsult}>
+          <Text style={styles.buttonText}>Insult</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={generateCompliment}>
+          <Text style={styles.buttonText}>Compliment</Text>
+        </TouchableOpacity>
+      </View>
+      {insult !== '' && (
+        <View style={styles.resultContainer}>
+          <Text style={styles.resultTitle}>Insult:</Text>
+          <Text style={styles.resultText}>{insult}</Text>
+        </View>
+      )}
+      {compliment !== '' && (
+        <View style={styles.resultContainer}>
+          <Text style={styles.resultTitle}>Compliment:</Text>
+          <Text style={styles.resultText}>{compliment}</Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +57,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F5FCFF',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 30,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#2196F3',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  resultContainer: {
+    marginVertical: 20,
+    alignItems: 'center',
+  },
+  resultTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  resultText: {
+    fontSize: 16,
+    fontStyle: 'italic',
+    textAlign: 'center',
   },
 });
